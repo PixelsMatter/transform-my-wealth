ed0baea5d51f67d2c9f0b7a83cb2dc3106d31726
Leon Burman シ <pixelsmatter1@gmail.com>
2021-07-18 00:45:48 +0100

    Add benefits component

---
+++ src/components/Benefits.js
@@ -0,0 +1,24 @@
+
+// Libraries
+import React from 'react'
+
+// Components
+import Container from './Container'
+
+const Benefits = () => (
+	<section className='benefits'>
+		<Container>
+			<h2 className='benefits__heading'>In This Video Masterclass You'll Discover:</h2>
+			<ul className='benefits__list'>
+				<li className='benefits__list-item'>The thinking mistake that costs business owners thousands that you can avoid</li>
+				<li className='benefits__list-item'>The biggest underused online asset that you can turn into an engine of revenue</li>
+				<li className='benefits__list-item'>A step-by-step guide to a huge shortcut to online partnerships that can get you turbo charged growth</li>
+				<li className='benefits__list-item'>How to create passive recurring revenue for your business so you can increase your income and reduce your stress</li>
+			</ul>
+			<p className='benefits__para'><strong>And lots LOTS more!</strong></p>
+			<p className='benefits__para'>This is a unique chance to learn what really works online and get a huge shortcut in your business progress</p>
+		</Container>
+	</section>
+)
+
+export default Benefits
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,14 @@ import React from 'react'
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
 import Intro from '../components/Intro'
+import Benefits from '../components/Benefits'
+
 
 const IndexPage = () => (
 	<Layout>
 		<SEO title='Transform My Business | Video Masterclass'/>
 		<Intro/>
+		<Benefits/>
 	</Layout>
 )
 
