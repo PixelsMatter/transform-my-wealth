c8a4c1894504d67af4d04bcf81860f3b0859c196
Leon Burman シ <pixelsmatter1@gmail.com>
2021-07-18 21:45:55 +0100

    Action more client feedback

--- src/components/Presenter.js
+++ src/components/Presenter.js
@@ -5,11 +5,15 @@ import React from 'react'
 // Components
 import Container from './Container'
 
+// Images
+import imgPath from '../img/thumbnail.jpg'
+
 const Presenter = () => (
 	<section className='presenter'>
 		<Container>
-			<h2 className='benefits__heading'>Your Masterclass Presenter</h2>
-			<p><a href="https://www.nickwealthall.com" target="_blank" rel="noreferrer">Nick Wealthall</a> founded and grew Transform My Poker to a 7 figure business selling to customers in over 60 countries.</p>
+			<h2>Your Masterclass Presenter</h2>
+			<img src={imgPath} alt='Headshot of Nick Wealthall' className='presenter__img'/>
+			<p><a href='https://www.nickwealthall.com' target='_blank' rel='noreferrer'>Nick Wealthall</a> founded and grew Transform My Poker to a 7 figure business selling to customers in over 60 countries.</p>
 			<p>He now trains, coaches and advises some of the biggest businesses in the UK including Savills plc, St. James' Place and Willis Towers Watson and many more.</p>
 		</Container>
 	</section>
