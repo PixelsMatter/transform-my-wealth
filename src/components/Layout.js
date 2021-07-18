2e330812434b3686ee219d5beaaadad2bff92aad
Leon Burman シ <pixelsmatter1@gmail.com>
2021-07-18 20:32:17 +0100

    Remove cookie consent

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types'
 
 // Components
 import SEO from '../components/SEO'
-import CookieConsent from './CookieConsent'
 import Footer from './Footer'
 import JsNotice from './JsNotice'
 
@@ -14,7 +13,6 @@ const Layout = ({ children }) => {
 	return (
 		<>
 			<SEO/>
-			<CookieConsent/>
 			<main>{children}</main>
 			<JsNotice/>
 			<Footer/>
