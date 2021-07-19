067542aa691e28f55ab55c43877f4c0fd9cbfc32
Leon Burman シ <pixelsmatter1@gmail.com>
2021-07-19 20:27:03 +0100

    Style masterclass component

--- src/components/Masterclass.js
+++ src/components/Masterclass.js
@@ -9,13 +9,17 @@ import Container from './Container'
 import Video from '../video/intro/feel-good-inc.mp4'
 
 const Masterclass = () => (
-	<section className="masterclass">
+	<section className='masterclass'>
 		<Container>
-			<h1 className='masterclass__heading'>How you can make more revenue online without wasting time or burning cash</h1>
+			<h1 className='masterclass__heading'>How I built Transform My Poker</h1>
+			<h2 className='masterclass__subheading'>*or how you can make more revenue online without wasting time or
+				burning cash</h2>
+
 			<video autoPlay muted loop className='masterclass__video'>
 				<source src={Video} type="video/mp4"/>
 			</video>
-			<p>Enjoy this video masterclass designed to help your online business</p>
+
+			<p>Enjoy this video masterclass designed to help your online business!</p>
 			<p>To let me know what you think and what you'd like help with next hit reply to my emails.</p>
 			<p>Thanks, <br/> Nick</p>
 		</Container>
